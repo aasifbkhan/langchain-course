28cff2e33425a37d5780d0b19210e8c525fb923f
Aasif Khan <aasifbkhan98@gmail.com>
2026-02-10 22:37:43 +0530

    added pydantic modal for structured output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -23,9 +26,22 @@ from langchain_tavily import TavilySearch
 #     print(f"Searching for {query}")
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent response with answer and sources"""
+
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
+
+
 llm = ChatOpenAI(model="gpt-5-mini")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the pune area on linkedin and list their details")})
